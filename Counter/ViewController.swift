4ffd5415da518b4e310a042bab865531b0d6d824
tik-tak-ololo <tik-tak-ololo@outlook.com>
2025-11-09 00:46:23 +0700

    sprint 03 base task completed

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var addOneButton: UIButton!
+    
+    private var counter: Int = 0 {
+        didSet {
+            counterLabel.text = "\(counter)"
+        }
+    }
+    
+    @IBAction func addOne(_ sender: UIButton) {
+        counter += 1
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
