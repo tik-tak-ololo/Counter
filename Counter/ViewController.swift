9d386d29704ecf5001b934e7ae42315f4d86ad1e
tik-tak-ololo <tik-tak-ololo@outlook.com>
2025-11-08 20:14:51 +0700

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Сергей Хмелёв on 08.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
